afc71d62ecad575425feb8629b8d081b169643bc
Takayama Fumihiko <tekezo@pqrs.org>
2018-12-22 16:20:57 +0900

    add make_number

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -3,7 +3,7 @@
 
 #include <pqrs/cf/number.hpp>
 
-TEST_CASE("cf_number") {
+TEST_CASE("make_cf_number") {
   {
     int8_t value = 100;
     auto number_ptr = pqrs::cf::make_cf_number(value);
@@ -51,5 +51,43 @@ TEST_CASE("cf_number") {
     REQUIRE(CFNumberGetByteSize(*number_ptr) == sizeof(value));
     REQUIRE(CFNumberGetValue(*number_ptr, kCFNumberFloat64Type, &value));
     REQUIRE(value == Approx(100.0));
+  }
+}
+
+TEST_CASE("make_number") {
+  {
+    int64_t value = 1099512677408LL; // 2^40 + 2^20 + 2^10 + 2^5
+    auto number_ptr = pqrs::cf::make_cf_number(value);
+
+    {
+      auto n = pqrs::cf::make_number<int8_t>(*number_ptr);
+      REQUIRE(n != std::nullopt);
+      REQUIRE(*n == static_cast<int8_t>(value));
+    }
+    {
+      auto n = pqrs::cf::make_number<int16_t>(*number_ptr);
+      REQUIRE(n != std::nullopt);
+      REQUIRE(*n == static_cast<int16_t>(value));
+    }
+    {
+      auto n = pqrs::cf::make_number<int32_t>(*number_ptr);
+      REQUIRE(n != std::nullopt);
+      REQUIRE(*n == static_cast<int32_t>(value));
+    }
+    {
+      auto n = pqrs::cf::make_number<int64_t>(*number_ptr);
+      REQUIRE(n != std::nullopt);
+      REQUIRE(*n == value);
+    }
+    {
+      auto n = pqrs::cf::make_number<float>(*number_ptr);
+      REQUIRE(n != std::nullopt);
+      REQUIRE(*n == Approx(static_cast<float>(value)));
+    }
+    {
+      auto n = pqrs::cf::make_number<double>(*number_ptr);
+      REQUIRE(n != std::nullopt);
+      REQUIRE(*n == Approx(static_cast<double>(value)));
+    }
   }
 }
