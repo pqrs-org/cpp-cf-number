d072238c8fe24261812cacaa63d3b83e0630e0ac
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-23 08:02:35 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,103 +1,108 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
+#include <boost/ut.hpp>
 #include <pqrs/cf/number.hpp>
 
-TEST_CASE("make_cf_number") {
-  {
-    int8_t value = 100;
-    auto number_ptr = pqrs::cf::make_cf_number(value);
-    REQUIRE(number_ptr);
-    REQUIRE(CFNumberGetByteSize(*number_ptr) == sizeof(value));
-    REQUIRE(CFNumberGetValue(*number_ptr, kCFNumberSInt8Type, &value));
-    REQUIRE(value == 100);
-  }
-  {
-    int16_t value = 100;
-    auto number_ptr = pqrs::cf::make_cf_number(value);
-    REQUIRE(number_ptr);
-    REQUIRE(CFNumberGetByteSize(*number_ptr) == sizeof(value));
-    REQUIRE(CFNumberGetValue(*number_ptr, kCFNumberSInt16Type, &value));
-    REQUIRE(value == 100);
-  }
-  {
-    int32_t value = 100;
-    auto number_ptr = pqrs::cf::make_cf_number(value);
-    REQUIRE(number_ptr);
-    REQUIRE(CFNumberGetByteSize(*number_ptr) == sizeof(value));
-    REQUIRE(CFNumberGetValue(*number_ptr, kCFNumberSInt32Type, &value));
-    REQUIRE(value == 100);
-  }
-  {
-    int64_t value = 100;
-    auto number_ptr = pqrs::cf::make_cf_number(value);
-    REQUIRE(number_ptr);
-    REQUIRE(CFNumberGetByteSize(*number_ptr) == sizeof(value));
-    REQUIRE(CFNumberGetValue(*number_ptr, kCFNumberSInt64Type, &value));
-    REQUIRE(value == 100);
-  }
-  {
-    float value = 100.0f;
-    auto number_ptr = pqrs::cf::make_cf_number(value);
-    REQUIRE(number_ptr);
-    REQUIRE(CFNumberGetByteSize(*number_ptr) == sizeof(value));
-    REQUIRE(CFNumberGetValue(*number_ptr, kCFNumberFloat32Type, &value));
-    REQUIRE(value == Approx(100.0f));
-  }
-  {
-    double value = 100.0;
-    auto number_ptr = pqrs::cf::make_cf_number(value);
-    REQUIRE(number_ptr);
-    REQUIRE(CFNumberGetByteSize(*number_ptr) == sizeof(value));
-    REQUIRE(CFNumberGetValue(*number_ptr, kCFNumberFloat64Type, &value));
-    REQUIRE(value == Approx(100.0));
-  }
-}
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
 
-TEST_CASE("make_number") {
-  {
-    auto n = pqrs::cf::make_number<int8_t>(nullptr);
-    REQUIRE(n == std::nullopt);
-  }
+  "make_cf_number"_test = [] {
+    {
+      int8_t value = 100;
+      auto number_ptr = pqrs::cf::make_cf_number(value);
+      expect(number_ptr);
+      expect(CFNumberGetByteSize(*number_ptr) == sizeof(value));
+      expect(CFNumberGetValue(*number_ptr, kCFNumberSInt8Type, &value));
+      expect(value == 100);
+    }
+    {
+      int16_t value = 100;
+      auto number_ptr = pqrs::cf::make_cf_number(value);
+      expect(number_ptr);
+      expect(CFNumberGetByteSize(*number_ptr) == sizeof(value));
+      expect(CFNumberGetValue(*number_ptr, kCFNumberSInt16Type, &value));
+      expect(value == 100);
+    }
+    {
+      int32_t value = 100;
+      auto number_ptr = pqrs::cf::make_cf_number(value);
+      expect(number_ptr);
+      expect(CFNumberGetByteSize(*number_ptr) == sizeof(value));
+      expect(CFNumberGetValue(*number_ptr, kCFNumberSInt32Type, &value));
+      expect(value == 100);
+    }
+    {
+      int64_t value = 100;
+      auto number_ptr = pqrs::cf::make_cf_number(value);
+      expect(number_ptr);
+      expect(CFNumberGetByteSize(*number_ptr) == sizeof(value));
+      expect(CFNumberGetValue(*number_ptr, kCFNumberSInt64Type, &value));
+      expect(value == 100);
+    }
+    {
+      float value = 100.0f;
+      auto number_ptr = pqrs::cf::make_cf_number(value);
+      expect(number_ptr);
+      expect(CFNumberGetByteSize(*number_ptr) == sizeof(value));
+      expect(CFNumberGetValue(*number_ptr, kCFNumberFloat32Type, &value));
+      expect(value == 100.0_f);
+    }
+    {
+      double value = 100.0;
+      auto number_ptr = pqrs::cf::make_cf_number(value);
+      expect(number_ptr);
+      expect(CFNumberGetByteSize(*number_ptr) == sizeof(value));
+      expect(CFNumberGetValue(*number_ptr, kCFNumberFloat64Type, &value));
+      expect(value == 100.0_d);
+    }
+  };
 
-  {
-    auto n = pqrs::cf::make_number<int8_t>(CFSTR("1234"));
-    REQUIRE(n == std::nullopt);
-  }
-
-  {
-    int64_t value = 1099512677408LL; // 2^40 + 2^20 + 2^10 + 2^5
-    auto number_ptr = pqrs::cf::make_cf_number(value);
+  "make_number"_test = [] {
+    {
+      auto n = pqrs::cf::make_number<int8_t>(nullptr);
+      expect(n == std::nullopt);
+    }
 
     {
-      auto n = pqrs::cf::make_number<int8_t>(*number_ptr);
-      REQUIRE(n != std::nullopt);
-      REQUIRE(*n == static_cast<int8_t>(value));
+      auto n = pqrs::cf::make_number<int8_t>(CFSTR("1234"));
+      expect(n == std::nullopt);
     }
+
     {
-      auto n = pqrs::cf::make_number<int16_t>(*number_ptr);
-      REQUIRE(n != std::nullopt);
-      REQUIRE(*n == static_cast<int16_t>(value));
+      int64_t value = 1099512677408LL; // 2^40 + 2^20 + 2^10 + 2^5
+      auto number_ptr = pqrs::cf::make_cf_number(value);
+
+      {
+        auto n = pqrs::cf::make_number<int8_t>(*number_ptr);
+        expect(n != std::nullopt);
+        expect(*n == static_cast<int8_t>(value));
+      }
+      {
+        auto n = pqrs::cf::make_number<int16_t>(*number_ptr);
+        expect(n != std::nullopt);
+        expect(*n == static_cast<int16_t>(value));
+      }
+      {
+        auto n = pqrs::cf::make_number<int32_t>(*number_ptr);
+        expect(n != std::nullopt);
+        expect(*n == static_cast<int32_t>(value));
+      }
+      {
+        auto n = pqrs::cf::make_number<int64_t>(*number_ptr);
+        expect(n != std::nullopt);
+        expect(*n == value);
+      }
+      {
+        auto n = pqrs::cf::make_number<float>(*number_ptr);
+        expect(n != std::nullopt);
+        expect(*n == static_cast<float>(value));
+      }
+      {
+        auto n = pqrs::cf::make_number<double>(*number_ptr);
+        expect(n != std::nullopt);
+        expect(*n == static_cast<double>(value));
+      }
     }
-    {
-      auto n = pqrs::cf::make_number<int32_t>(*number_ptr);
-      REQUIRE(n != std::nullopt);
-      REQUIRE(*n == static_cast<int32_t>(value));
-    }
-    {
-      auto n = pqrs::cf::make_number<int64_t>(*number_ptr);
-      REQUIRE(n != std::nullopt);
-      REQUIRE(*n == value);
-    }
-    {
-      auto n = pqrs::cf::make_number<float>(*number_ptr);
-      REQUIRE(n != std::nullopt);
-      REQUIRE(*n == Approx(static_cast<float>(value)));
-    }
-    {
-      auto n = pqrs::cf::make_number<double>(*number_ptr);
-      REQUIRE(n != std::nullopt);
-      REQUIRE(*n == Approx(static_cast<double>(value)));
-    }
-  }
+  };
+
+  return 0;
 }
